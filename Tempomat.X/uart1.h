c96e7d3bf046a55e0a09bc30c928bd77b96c25fe
salemilankov <salemilankov@gmail.com>
2015-06-09 20:19:24 +0200

    Revert "brisem"
    This reverts commit dc65283c8d5472281ccc9bc8f4e2c89a4af5f883.

---
+++ Tempomat.X/uart1.h
@@ -0,0 +1,3 @@
+void initUART1(void);//funkciija za inicijalizaciju UARTa
+void writeUART1(unsigned int);//funkcija koja salje karakter na PC
+void writeUART1decToString(unsigned int);//funkcija koja decimalan broj pretvara u string i salje ga na PC
